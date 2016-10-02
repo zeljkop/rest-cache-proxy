77abeb776b60ab843788574950e93f0e57f23d27
Željko Pušec <zeljko.pusec@gmail.com>
2016-10-02 23:23:55 +0200

    working spring boot application

---
+++ rest-cache-proxy/src/main/java/com/fexco/app/App.java
@@ -0,0 +1,17 @@
+package com.fexco.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zpusec
+ * Spring boot entry point.
+ */
+@SpringBootApplication
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ rest-cache-proxy/src/main/java/com/fexco/config/Config.java
@@ -0,0 +1,13 @@
+package com.fexco.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author zpusec
+ * Base configuration class, various bean declaration.
+ */
+@Configuration
+public class Config {
+
+}
